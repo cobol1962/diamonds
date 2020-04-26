c2815a0f0c4e8447fb1aa0dcd2e9c1af7d9185ab
cobol1962 <cobol1962@gmail.com>
2020-04-26 10:21:13 +0200

    2222

--- platforms/browser/www/js/mainScript.js
+++ platforms/browser/www/js/mainScript.js
@@ -16,7 +16,7 @@ var spersons = [];
 var firstCatalog = true;
 var firstDiamond = true;
 var optionsLoader = {
-  image: "http://85.214.165.56/coster/www/images/diamond.gif",
+  image: "http://85.214.165.56:81/coster/www/images/diamond.gif",
   imageAnimation: false
 }
 function resetLocalStorage() {
